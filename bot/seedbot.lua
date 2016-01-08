07ce8055e18055c686324a805c435264ceca26f3
MoLtafet <matin_mo_14@yahoo.com>
2016-01-08 16:01:15 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {149223247},--Sudo users
+    sudo_users = {149223247,159769161},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
